8c3877cb537f077d24c9e99d12b32c6890781a81
George Kaluwin <georgekaluwin@gmail.com>
2022-04-11 20:55:09 +0930

    scheduler working but styling needs work

---
+++ script.js
@@ -0,0 +1,41 @@
+// 
+$(init);
+
+// dynamic display of date and time at the top of the page
+function init(){
+    $("#currentDay").text(moment().format("dddd,MMMM Do YYYY, H:mm"));
+
+// Timeblocks to be checked every minute for colour updates
+timeBlocks();
+setInterval(timeBlocks,60000);
+
+$(".time-row").each(function(){
+    var block= $(this).attr("id");
+    $("#" + block + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + block));
+});
+
+
+$(".saveBtn").on("click",handleSave);
+}
+
+function timeBlocks(){
+
+    $(".time-row").each(function(){
+        var blockTime=parseInt($(this).attr("id").replace("hour-", ""));
+        var currentTime= parseInt(moment().format("H"));
+
+        if (blockTime<currentTime){
+            $(this).addClass("past");
+        } else if (blockTime>currentTime){
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    
+    });
+}
+
+function handleSave(event){
+    var hourId=$(this).parent().attr("id");
+    localStorage.setItem(moment().format("DDDYYYY")+hourId, $("#" + hourId + " textarea").val());
+}
